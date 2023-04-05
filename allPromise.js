b4003f045e9cbd0d474b42c6ad0bbf2f0d922088
Shahith R <87928073+Shahithraj@users.noreply.github.com>
2023-04-05 23:35:32 +0530

    Create allPromise.js

---
+++ allPromise.js
@@ -0,0 +1,38 @@
+const promise1 = new Promise((resolve, reject) => {
+  resolve('success');
+});
+
+const promise2 = new Promise((resolve, reject) => {
+  reject('success2');
+});
+
+const promise3 = new Promise((resolve, reject) => {
+  setTimeout(() => resolve('happy'), 100);
+});
+
+const allPromises = [promise1, promise2, promise3];
+
+Promise.all(allPromises)
+  .then((values) => console.log(values))
+  .catch((err) => console.log(err, 'error'));
+
+// 1. Promise all
+
+//  * If one of the promise is rejected, it will print the reject statement in the catch block and other will not be executed.
+
+Promise.allSettled(allPromises).then((result) => console.log(result));
+
+// 2. Promise allSettled
+
+// It will return all the Promise status and value in array of object. Eg:
+// [
+//   { status: 'fulfilled', value: 'success' },
+//   { status: 'rejected', reason: 'success2' },
+//   { status: 'fulfilled', value: 'happy' }
+// ]
+
+// 3. Promise race
+
+Promise.race(allPromises).then((value) => console.log(value));
+
+// It will return first executing promise either it is rejected or resolved in then block
